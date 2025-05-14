b742cf6574b8b73f6bbf99ff1103903336f5112a
SHINJAGIRA <shinjagiraarnold209@gmail.com>
2025-05-14 09:31:09 +0200

    finish

--- src/app/page.js
+++ src/app/page.js
@@ -60,7 +60,7 @@ export default function Home() {
     formData.append('file', image);
 
     try {
-      const response = await axios.post('http://127.0.0.1:4000/predict', formData, {
+      const response = await axios.post('http://0.0.0.0:8080/predict', formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
       setPrediction(response.data.label);
